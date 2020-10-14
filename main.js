72212599bb2470437cf0ae7a60756292c2c7f9f1
Ian Walls <wallsianh@gmail.com>
2020-10-14 16:37:45 -0400

    added checkDead to end game

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ function jump () {
         character.classList.remove("animate");
     }, 500)
 }
+
+const checkDead = setInterval(function(){
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You Lose")
+    }
+},10)
